5150356dae58f55b2dc8f53390f19038819f6287
bengu <bnguyilmaz@gmail.com>
2026-03-24 22:34:41 +0300

    controllers and html pages are added for home and music page

---
+++ src/main/java/com/bengu/springblog/controllers/MusicController.java
@@ -0,0 +1,14 @@
+package com.bengu.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/music")
+public class MusicController {
+    @GetMapping
+    public String music(){
+        return "music";
+    }
+}
