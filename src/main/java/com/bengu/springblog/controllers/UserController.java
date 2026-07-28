2de687915f70070e61ee932ac67780cc91448873
Bengü Yılmaz <bnguyilmaz@gmail.com>
2026-07-28 09:13:49 +0300

    User controller has been added.

---
+++ src/main/java/com/bengu/springblog/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.bengu.springblog.controllers;
+
+import com.bengu.springblog.entities.User;
+import com.bengu.springblog.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(
+            @RequestBody User user
+    ) {
+        User createdUser = userService.createUser(user);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdUser);
+    }
+}
